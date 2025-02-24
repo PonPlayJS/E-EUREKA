7357073486ab7e792e83b0805ae9192ffe2fcb52
sebasplays <sebastianvargas3500@gmail.com>
2025-02-24 12:02:13 -0300

    Update view.py

--- view.py
+++ view.py
@@ -2,19 +2,21 @@ import gymnasium as gym
 from stable_baselines3 import PPO
 
 # Cargar el modelo desde la ubicación (puedes cambiarla)
-model_path = "/home/joaco/Escritorio/vscode/cartpole_ppo"
-model = PPO.load(model_path)
+model_path = "/home/joaco/Escritorio/vscode/cartpole_ppo" # Ruta donde se encuentra el modelo guardado
+model = PPO.load(model_path) # Carga el modelo PPO previamente entrenado
 
-# "crea" el render
+# Crear el entorno de CartPole con renderización en modo "human" (renderizar para visualización humana)
 env = gym.make("CartPole-v1", render_mode="human")
-observation, info = env.reset()
+observation, info = env.reset() # Reinicia el entorno y obtiene la observación inicial
 
 # Ejecutar política aprendida
 for _ in range(1000):
-    action, _ = model.predict(observation)
+    action, _ = model.predict(observation) # Predecir la acción a tomar según la observación actual
     observation, reward, terminated, truncated, info = env.step(action)
-    
+
+     # Si el episodio ha terminado o ha sido truncado, reinicia el entorno
     if terminated or truncated:
         observation, info = env.reset()
 
+# Cerrar el entorno para liberar recursos
 env.close()
